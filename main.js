e44ea67d545a28b6a2b6539bbab6f9753285d0df
mdorena <mdorena@sscgi.com>
2024-12-31 11:51:42 +0800

    Finished section 1, entrance animation and custom cursor

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ buttonMode.addEventListener("click", function () {
   const lightClasses = document.querySelectorAll(".light");
   const lightBgClasses = document.querySelectorAll(".light-bg");
   const darkBgClasses = document.querySelectorAll(".dark-bg");
+  const lightReverseBgClasses = document.querySelectorAll(".light-reverse-bg");
+  const darkReverseBgClasses = document.querySelectorAll(".dark-reverse-bg");
   const darkClasses = document.querySelectorAll(".dark");
   const iconMode = document.getElementById("icon-mode");
   if (darkClasses.length == 0) {
@@ -55,6 +57,12 @@ buttonMode.addEventListener("click", function () {
       // Add the "dark" class
       lightBgClasses[i].classList.add("dark-bg");
     }
+    for (var i = 0; i < lightReverseBgClasses.length; i++) {
+      // Remove the "light" class
+      lightReverseBgClasses[i].classList.remove("light-reverse-bg");
+      // Add the "dark" class
+      lightReverseBgClasses[i].classList.add("dark-reverse-bg");
+    }
   } else {
     iconMode.classList.remove("fa-sun");
     iconMode.classList.add("fa-moon");
@@ -70,5 +78,48 @@ buttonMode.addEventListener("click", function () {
       // Add the "dark" class
       darkBgClasses[i].classList.add("light-bg");
     }
+    for (var i = 0; i < darkReverseBgClasses.length; i++) {
+      // Remove the "light" class
+      darkReverseBgClasses[i].classList.remove("dark-reverse-bg");
+      // Add the "dark" class
+      darkReverseBgClasses[i].classList.add("light-reverse-bg");
+    }
   }
 });
+
+// for animation
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// custoom cursor
+const mouse1 = document.querySelector(".mouseef");
+const mouse2 = document.querySelector(".outline");
+document.body,
+  addEventListener("mousemove", (event) => {
+    mouse1.style.left = `${event.x}px`;
+    mouse1.style.top = `${event.y}px`;
+    // mouse2.style.left = `${event.x}px`
+    // mouse2.style.top = `${event.y}px`
+
+    mouse2.animate(
+      {
+        left: `${event.x}px`,
+        top: `${event.y}px`,
+      },
+      {
+        duration: 500,
+        fill: "forwards",
+      }
+    );
+  });
